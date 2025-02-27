27613f45713cb9e8a7d9b7961b31e9f2744b863a
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-02-27 20:13:31 +0530

    Added_BattleShip_Console

---
+++ ConsoleApps/BattleShip/Models/Position.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMCard.Models
+{
+    class Position
+    {
+        public int x { get; set; } = -1;
+        public int y { get; set; } = -1;
+    }
+}
